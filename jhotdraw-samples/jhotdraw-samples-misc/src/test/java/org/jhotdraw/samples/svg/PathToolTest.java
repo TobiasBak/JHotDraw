5311b2d2b368fbfefc2c486feddcfceffe18799c
Kevin Torp <Kevintorp@hotmail.com>
2024-01-02 16:18:53 +0100

    Added test classes

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/PathToolTest.java
@@ -0,0 +1,14 @@
+package org.jhotdraw.samples.svg;
+
+import junit.framework.TestCase;
+
+public class PathToolTest extends TestCase {
+
+    public void testCreatePath() {
+        //Create test for createPath
+
+    }
+
+    public void testFinishCreation() {
+    }
+}
---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
@@ -0,0 +1,27 @@
+package org.jhotdraw.samples.svg.figures;
+
+import junit.framework.TestCase;
+
+public class SVGPathFigureTest extends TestCase {
+
+    public void testDraw() {
+    }
+
+    public void testDrawBufferedImage() {
+    }
+
+    public void testDrawFigure() {
+    }
+
+    public void testDrawChildren() {
+    }
+
+    public void testGetDrawingArea() {
+    }
+
+    public void testContains() {
+    }
+
+    public void testTestClone() {
+    }
+}
