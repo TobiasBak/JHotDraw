e4c506011a21d51f20bedec32f710df8da5b5464
Kevin Torp <Kevintorp@hotmail.com>
2024-01-02 18:00:58 +0100

    Setup and teardown created

--- jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
@@ -4,19 +4,57 @@ import junit.framework.TestCase;
 
 public class SVGPathFigureTest extends TestCase {
 
+    SVGPathFigure svgPathFigure;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        svgPathFigure = new SVGPathFigure();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        svgPathFigure = null;
+    }
+
     public void testDraw() {
+        //Setup
+
+        //Run the test
+
+        //Verify the results
+
     }
 
     public void testDrawBufferedImage() {
+        //Setup
+
+        //Run the test
+
+        //Verify the results
     }
 
     public void testContains() {
+        //Setup
+
+        //Run the test
+
+        //Verify the results
     }
 
     public void testDrawFigure() {
+        //Setup
+
+        //Run the test
+
+        //Verify the results
     }
 
     public void testGetDrawingArea() {
+        //Setup
+
+        //Run the test
+
+        //Verify the results
     }
 
 }
